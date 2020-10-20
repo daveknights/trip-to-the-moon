69ddbe45c224ff056d011ae6abd6ad05253c107f
David Knights <david.knights@vcars.co.uk>
2020-10-20 12:56:53 +0100

    Initial commit of Choreographer JS 'Trip to the moon' project

---
+++ script.js
@@ -0,0 +1,58 @@
+let choreographer = new Choreographer({
+  animations: [
+    {
+        range: [-1, 1500],
+        selector: '#moon',
+        type: 'change',
+        style: 'opacity',
+        from: 0,
+        to: 1,
+    },
+    {
+      range: [-1, 1500],
+      selector: '#moon',
+      type: 'scale',
+      style: 'transform:scale',
+      from: 0,
+      to: 1
+    },
+    {
+      range: [-1, 1500],
+      selector: '#bullet',
+      type: 'scale',
+      style: 'left',
+      from: 0,
+      to: 60,
+      unit: '%'
+    },
+    {
+      range: [-1, 1500],
+      selector: '#bullet',
+      type: 'scale',
+      style: 'top',
+      from: 0,
+      to: 420,
+      unit: 'px'
+    },
+    {
+      range: [1501, 2500],
+      selector: '#bullet',
+      type: 'change',
+      style: 'opacity',
+      from: 1,
+      to: 0
+    },
+    {
+      range: [1501, 2500],
+      selector: '#bullet-moon',
+      type: 'change',
+      style: 'opacity',
+      from: 0,
+      to: 1
+    }
+  ]
+});
+
+window.addEventListener('scroll', () => {
+  choreographer.runAnimationsAt(window.pageYOffset)
+});
